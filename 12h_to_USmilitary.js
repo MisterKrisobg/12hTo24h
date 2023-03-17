f07dec87aeb3be8e5e9a951cd876f3b24d4fbfcc
MisterKrisobg <74677202+MisterKrisobg@users.noreply.github.com>
2023-03-17 12:25:25 +0000

    More error checks, comments and consistency

--- 12h_to_USmilitary.js
+++ 12h_to_USmilitary.js
@@ -1,47 +1,66 @@
-function convertTimeUS(String) {
-    let AM = String.includes("AM");
-    let PM = String.includes("PM");
-    if (AM == true) {
-        let timE = String.replace("AM", "");
-        timE = timE.replace(":", "");
-        timE = +timE
-        let Time = timE
-        if (timE < 1000) {
-            timE = timE.toString();
-            timE = "0" + timE;
-            Time = timE;
-            console.log(Time);
-        }
-        else if (timE >= 1200 && timE <=1259) {
-            timE = timE.toString();
-            timE = timE.slice(2, 4);
-            timE = "00" + timE;
-            Time = timE;
-            console.log(Time);
-        }
-        else if (timE > 1259){
-            console.log("Invalid time entered.")
-        }
-    } 
-    else if (PM == true) {
-        let time = String.replace("PM", "");
-        time = time.replace(":", "");
-        Time = +time;
-        if (Time <= 1159) {
-            Time += 1200;
-            console.log(Time);
-        }
-        else if (Time >=1200 && Time <= 1259) {
-            console.log(Time);
-        }
-        else {
-            console.log("Invalid time entered.")
-        }
+// 12 hour to US Military Time converter
+function convertTimeUS(input) 
+{
+    let AM = input.includes("AM");
+    let PM = input.includes("PM");
+    if (!AM && !PM) 
+    {
+        console.log("Make sure the value you enter contains AM or PM.");
+        return;
     }
-    else {
-        console.log("The value you entered isn't valid.");
+    if (AM && PM) 
+    {
+        console.log("Make sure the value you enter does not contain both AM or PM.");
+        return;
+    }
+    if (AM) 
+    {
+        // enables time's conversion into an int for validty checks
+        let time = input.replace("AM", "");
+        time = time.replace(":", "");
+        time = +time;
+        // AM and PM can't be above 12:59 or under 1:00
+        if (time > 1259 || time <= 59) 
+        {
+            console.log("Invalid input");
+            return;
+        }
+
+        if (time < 1000) 
+        {
+            time = time.toString();
+            time = "0" + time;
+        }
+        // 12AM is interperted as 0000
+        else if (time >= 1200) 
+        {
+            time = time.toString();
+            time = time.slice(2, 4);
+            time = "00" + time; 
+        }
+        console.log(time);
+        return time;
+    } 
+    else if (PM) 
+    {
+        // enables time's conversion into an int for validty checks
+        let time = input.replace("PM", "");
+        time = time.replace(":", "");
+        time = +time;
+    // AM and PM can't be above 12:59 or under 1:00
+        if (time > 1259 || time <= 59) 
+        {
+            console.log("Invalid input");
+            return;
+        }
+        // Don't add 1200 if it's 12PM
+        if (time <= 1159) 
+        {
+            time += 1200;
+        }
+        console.log(time);
     }
 }
 // Example
-convertTimeUS("11:34 AM");
-// Expected result is 1134
+convertTimeUS("12:00 AM");
+// Expected output: 1200
